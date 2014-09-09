de56be8205babd0da3d7afd07ad779674f819836
Adam Killander <adam.killander@adaptiv.se>
2014-09-09 12:18:14 +0200

    Fixar Eclipseprojektet så att det använder Cucumber

--- pmlib-outsidein-demo-end/test/se/tddcourse/pmlib/scenarios/EmptyMovieLibrarySteps.java
+++ pmlib-outsidein-demo-end/test/se/tddcourse/pmlib/scenarios/EmptyMovieLibrarySteps.java
@@ -3,9 +3,6 @@ package se.tddcourse.pmlib.scenarios;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
 import cucumber.api.java.sv.Givet;
 import cucumber.api.java.sv.När;
 import cucumber.api.java.sv.Så;
